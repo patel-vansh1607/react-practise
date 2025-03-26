31b0e321395a423fb3e802f6b72cba163666cd91
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-26 17:26:10 +0300

    add registration form component to the main application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/form';
+import RegForm from './components/RegForm';
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <RegForm />
     </div>
   );
 }
