2802c65114aa82fd62ee67b5e010dd69a52b6be1
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-27 15:23:00 +0300

    add footer component to the main application

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './components/form';
 import RegForm from './components/RegForm';
+import FooterComp from './components/footerComp';
 
 function App() {
   return (
     <div className="App">
       <Form />
       <RegForm />
+      <FooterComp />
     </div>
   );
 }
