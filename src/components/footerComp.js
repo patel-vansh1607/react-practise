b6630bb72f6c62b1d08b719eeaae0a98524c0ed3
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-27 15:31:05 +0300

    add footer component to display copyright information

---
+++ src/components/footerComp.js
@@ -0,0 +1,9 @@
+const FooterComp = () => {
+    return(
+        <div>
+            <p>All Rights Reserved</p>
+            <span>2025 @ Vansh Patel</span>
+        </div>
+    )
+}
+export default FooterComp       
